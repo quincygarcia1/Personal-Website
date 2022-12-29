ce2950663cdd9be103933b2916e84c615dc31694
Quincy Garcia <quincygarcia@Quincys-MacBook-Air.local>
2022-12-28 23:40:39 -0500

    Additional viewport reliance changes

--- src/App.js
+++ src/App.js
@@ -4,32 +4,36 @@ import MountainRange from './VisualComponents/MountainRange';
 import InfoSection from './Components/InfoSection';
 import classes from "../src/Modules/EnvironmentGraphics.module.css"
 import NavigationBar from './Components/NavigationBar';
+import {Helmet} from "react-helmet"
 
 function App() {
-
   return (
-    <div className='non-selectable'>
-      <div className='background' style={{cursor: 'none'}}>
-      </div>
-      <div className='topLayer'>
-        <div className='nameContainer noCursor'>
-          <div className='name'>Hi, I'm Quincy</div>
+      <div className='non-selectable'>
+        <Helmet>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        </Helmet>
+        <div className='background' style={{ cursor: 'none' }}>
         </div>
-        
-        
-        <img className={classes.deer} src='deer1.png' alt='deer' width="200"></img>
-        <Sun />
-        <Land />
-        <MountainRange />
-        <NavigationBar></NavigationBar>
-      </div>
-      
+        <div className='topLayer'>
+          <div className='nameContainer noCursor'>
+            <div className='name'>Hi, I'm Quincy</div>
+          </div>
+          <div className='sunInteraction'>
+            <Sun />
+          </div>
 
-      <div className='bodyContainer'>
-        <InfoSection/>
+          <img className={classes.deer} src='deer1.png' alt='deer' width="200"></img>
+          
+          <Land />
+          <MountainRange />
+          <NavigationBar></NavigationBar>
+        </div>
+
+
+        <div className='bodyContainer'>
+          <InfoSection />
+        </div>
       </div>
-    </div>
-    
   );
 }
 
